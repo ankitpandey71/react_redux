54719816f9162e08628baf0bb792d43574f7cd85
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-02-08 17:37:49 +0530

    added cart

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,23 @@
 import React from "react";
+import "../App.css";
 
 function Home() {
   return (
     <div>
+      <div className="cart">
+        <img src="/cart.png" height="50px" alt="" />{" "}
+      </div>
       <h1>Home components</h1>
       <div>
-        <img src="" />
+        <img src="/aa.jpg" height="100px" alt="" />
+      </div>
+      <div>
+        <span>I-Phone</span>
+        <br />
+        <span>Price:</span>
+      </div>
+      <div>
+        <button>Add to cart</button>
       </div>
     </div>
   );
