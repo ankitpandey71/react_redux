b3b290101ca3b3072a29b274451d8c6fd8fa7e8a
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-02-08 16:46:27 +0530

    added redux container & Services

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home components</h1>
+      <div>
+        <img src="" />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
